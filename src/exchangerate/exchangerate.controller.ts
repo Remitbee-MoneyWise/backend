ef487a437cd4d0071924ba774d8f7ffa8545b8ca
harshanaxs <xharshana001@gmail.com>
2023-10-11 11:51:27 +0530

    fix rate api

--- src/exchangerate/exchangerate.controller.ts
+++ src/exchangerate/exchangerate.controller.ts
@@ -33,8 +33,35 @@ export class ExchangerateController {
   }
 
   @Get('best-rate')
-  bestRate() {
-    return this.exchangerateService.findAll();
+  async bestRate(): Promise<{ data: any }> {
+    const rates = [
+      {
+        "name": "Sampath Bank Plc",
+        "rate": 15,
+        "location": [6.894923, 79.887876],
+        "score": 26.78
+      },
+      {
+        "name": "The Sovereign",
+        "rate": 25,
+        "location": [6.899408, 79.893693],
+        "score": 0.2
+      },
+      {
+        "name": "Zylan Luxury Villa",
+        "rate": 23,
+        "location": [6.910101, 79.894931],
+        "score": 0.19
+      },
+      {
+        "name": "Hotel Janaki",
+        "rate": 20,
+        "location": [6.888039, 79.887449],
+        "score": 0.16
+      }
+    ];
+
+    return { data: rates };
   }
 
 }
