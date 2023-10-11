6f769fad744fdb31053dda7f559a15fd5ab42675
harshanaxs <xharshana001@gmail.com>
2023-10-11 12:24:55 +0530

    fix

--- src/exchangerate/exchangerate.service.ts
+++ src/exchangerate/exchangerate.service.ts
@@ -1,30 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { CreateExchangerateDto } from './dto/create-exchangerate.dto';
 import { UpdateExchangerateDto } from './dto/update-exchangerate.dto';
-
 @Injectable()
 export class ExchangerateService {
   create(createExchangerateDto: CreateExchangerateDto) {
     return 'This action adds a new exchangerate';
   }
-
   findAll() {
     return `This action returns all exchangerate`;
   }
-
   findOne(id: number) {
     return `This action returns a #${id} exchangerate`;
   }
-
   update(id: number, updateExchangerateDto: UpdateExchangerateDto) {
     return `This action updates a #${id} exchangerate`;
   }
-
   remove(id: number) {
     return `This action removes a #${id} exchangerate`;
   }
-
-  bestrate() {
+  bestRate() {
     const rates = [
       {
         "name": "Sampath Bank Plc",
@@ -54,5 +48,5 @@ export class ExchangerateService {
   
     console.log("Rates", rates);
   
-    return { data: rates };  }
-}
+    return { data: rates };
+  }
